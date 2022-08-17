4bdb33fa5ea23d32c05f9857631ab916bc957884
Krzysztof Krawczyk <krzysztofskrawczyk@gmail.com>
2022-08-17 21:19:04 +0200

    Removed unused imports

--- src/main/java/com/bots/RacoonServer/JdaManager.java
+++ src/main/java/com/bots/RacoonServer/JdaManager.java
@@ -1,16 +1,13 @@
 package com.bots.RacoonServer;
 
-import com.bots.RacoonServer.Controllers.EventListenerController;
 import com.bots.RacoonServer.Logging.Loggers.Logger;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
-import net.dv8tion.jda.api.hooks.AnnotatedEventManager;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 import net.dv8tion.jda.api.utils.ChunkingFilter;
 import net.dv8tion.jda.api.utils.MemberCachePolicy;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.DependsOn;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
