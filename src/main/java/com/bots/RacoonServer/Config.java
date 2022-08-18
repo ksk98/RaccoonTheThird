6c0da0249844974cdff0ff33bd48b3bcc5f174b6
Krzysztof Krawczyk <krzysztofskrawczyk@gmail.com>
2022-08-18 11:14:45 +0200

    Removed _ from constant name

--- src/main/java/com/bots/RacoonServer/Config.java
+++ src/main/java/com/bots/RacoonServer/Config.java
@@ -1,6 +1,6 @@
 package com.bots.RacoonServer;
 
 public abstract class Config {
-    public static final char[] _commandPrefixes = new char[]{'!', '$'};
+    public static final char[] commandPrefixes = new char[]{'!', '$'};
     public static final boolean deleteMessagesContainingOnlyValidCommandCall = true;
 }
