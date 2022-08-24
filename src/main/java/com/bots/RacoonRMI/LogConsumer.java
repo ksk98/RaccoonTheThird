de8faf0ec7e01241e3dacc0037797b63d1752eed
Krzysztof Krawczyk <krzysztofskrawczyk@gmail.com>
2022-08-24 11:51:09 +0200

    Deleted unused classes

--- src/main/java/com/bots/RacoonRMI/LogConsumer.java
+++
@@ -1,8 +0,0 @@
-package com.bots.RacoonRMI;
-
-public interface LogConsumer {
-    void log(String message, String caller);
-    void logError(String message, String caller);
-    void logSuccess(String message, String caller);
-    void logInfo(String message, String caller);
-}
