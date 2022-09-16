4064c73bc6e10a8b8d3facf8ab3027102ccc727f
Krzysztof Krawczyk <krzysztofskrawczyk@gmail.com>
2022-09-16 16:38:28 +0200

    Fix for server-channel list

--- src/main/java/com/bots/RaccoonServer/Services/BotIntelService.java
+++ src/main/java/com/bots/RaccoonServer/Services/BotIntelService.java
@@ -3,6 +3,7 @@ package com.bots.RaccoonServer.Services;
 import com.bots.RaccoonShared.Discord.Channel;
 import com.bots.RaccoonShared.Discord.ServerChannels;
 import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.entities.GuildMessageChannel;
 import org.springframework.stereotype.Service;
 
 import java.util.LinkedList;
@@ -23,9 +24,10 @@ public class BotIntelService {
     public LinkedList<ServerChannels> getServerChannelList() {
         if (!cacheValid) {
             cachedList = jda.getGuilds().stream().map(guild -> {
-                List<Channel> channels = guild.getTextChannels().stream().map(
-                        textChannel -> new Channel(textChannel.getId(), textChannel.getName())
-                ).toList();
+                List<Channel> channels = guild.getTextChannels().stream()
+                        .filter(GuildMessageChannel::canTalk)
+                        .map(textChannel -> new Channel(textChannel.getId(), textChannel.getName()))
+                        .toList();
 
                 return new ServerChannels(guild.getId(), guild.getName(), channels);
             }).collect(Collectors.toCollection(LinkedList::new));
