7025b41a7a501b26efdd0c13586d46510246b5e6
Krzysztof Krawczyk <krzysztofskrawczyk@gmail.com>
2022-09-23 00:33:23 +0200

    Removed unused import/annotation

--- src/main/java/com/bots/RaccoonServer/RacoonApplication.java
+++ src/main/java/com/bots/RaccoonServer/RacoonApplication.java
@@ -1,11 +1,9 @@
 package com.bots.RaccoonServer;
 
-import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableEncryptableProperties
 public class RacoonApplication {
 
 	public static void main(String[] args) {
